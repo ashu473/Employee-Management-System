687068535fd4ecb40e0198a9fa448b5f51dfce4d
Ashutosh Kumar <84832754+ashu473@users.noreply.github.com>
2023-09-30 01:13:31 +0530

    Add files via upload

---
+++ Project_Emp_Management/EmployeeManagement/src/main/java/com/example/EmployeeManagement/service/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.example.EmployeeManagement.service;
+
+import com.example.EmployeeManagement.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    List<Employee> getAllEmployees();
+    void saveEmployee(Employee employee);
+    Employee getEmployeeById(long id);
+    void deleteEmployeeById(long id);
+}
